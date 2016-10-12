281bf4499142216375a42904aa1976cfbf690509
Michael Rubinstein <michael@polyblog.com>
2016-10-11 23:12:40 -0700

    initial Firebase configuration stuff

---
+++ js/app.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2016 Michael N. Rubinstein. All Rights Reserved.
+ * 
+ * HYLOSPHERE
+ */
+
+// Initialize Firebase.
+var config = {
+  apiKey: "AIzaSyD8d4Q4mOI-RCh4zY7XfxUa_G4BfevAgOg",
+  authDomain: "hylosphere.firebaseapp.com",
+  databaseURL: "https://hylosphere.firebaseio.com",
+  storageBucket: "hylosphere.appspot.com",
+  messagingSenderId: "547167037002"
+};
+firebase.initializeApp(config);
